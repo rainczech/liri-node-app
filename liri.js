98398fb1f49865ab53cbc0059b4a8a658110c8bb
Grace Ho <rainczech@gmail.com>
2018-10-31 20:23:29 -0500

    added switch function to determine liri functions

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 // Dependencies
+
 // environment variables like secrets and api keys
 require('dotenv').config();
 // importing the keys.js file
@@ -10,9 +11,45 @@ const request = require('request');
 // spotify with my ID and secret
 const spotify = new Spotify(keys.spotify);
 
+
+
+
+// Functions
+
 // 4 commands
 // concert this
 // spotify-this-song
 // movie-this
 // do-what-it-says
 
+
+
+// switch case to execute the command
+const choose = function(operation, nameOfMedia){
+  switch(operation){
+    case 'concert-this':
+      getBands(nameOfMedia);
+      break;
+    case 'spotify-this-song':
+      spotifyThis(nameOfMedia);
+      break;
+    case 'movie-this':
+      movieThis(nameOfMedia);
+      break;
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+    default:
+      console.log("That is not a valid command. Try again.");
+  }
+};
+
+
+// get the input from the CLI run which function
+const run = function(input1, input2){
+  choose(input1, input2);
+}
+
+
+// Process
+run(process.argv[2], process.argv[3]);
